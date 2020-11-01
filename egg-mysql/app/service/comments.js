b2913e4c0b24c02c4a93e6e73c8a621757e01c77
LR0607 <734477156@qq.com>
2020-11-01 14:43:39 +0800

    14:43

--- egg-mysql/app/service/comments.js
+++ egg-mysql/app/service/comments.js
@@ -2,7 +2,7 @@ const Service = require('egg').Service;
 
 class CommentsService extends Service {
 	async comment(data) {
-		var sql = `insert into comment(content，uid，rid) values('${data.comment}',${data.uid},${data.rid}) `
+		var sql = `insert into comment(content，uid，rid,addtimes) values('${data.comment}',${data.uid},${data.rid},Now()) `
 		var result = await this.app.mysql.query(sql);
 		if (result.affectedRows) {
 			result = {
